bdcf6114e63205952725d60507fe0bcf00606b5a
Humberto Gonçalves <humberto.g.moreira@gmail.com>
2020-04-09 18:24:03 -0300

    Add redux Dev tools lib

--- src/store/enhancer/enhancer.js
+++ src/store/enhancer/enhancer.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import middlewares from '../middlewares'
 
 const enhancer =
   process.env.NODE_ENV === 'development'
-    ? compose(applyMiddleware(...middlewares))
+    ? compose(composeWithDevTools(applyMiddleware(...middlewares)))
     : applyMiddleware(...middlewares)
 
 export default enhancer
